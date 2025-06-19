a724f1e91333af4820649f02c54f38aa927b261e
Javier-Cs <krchipullajavier@gmail.com>
2025-06-18 23:43:02 -0500

    Creación de una clase para la excepción personalizada. -- Modificación de ProductServiceImpl -- uso de la notación @transactional para los métodos crud. -- uso de readOnly=true en la notación @Transactional para los métodos de solo lectura - Modificación del método update para evitar errores al no actualizar todos los datos.

--- src/main/java/com/ejeplo/productservice/Service/ProductServiceImpl.java
+++ src/main/java/com/ejeplo/productservice/Service/ProductServiceImpl.java
@@ -1,9 +1,10 @@
 package com.ejeplo.productservice.Service;
 
 import com.ejeplo.productservice.Entity.ProductEntity;
+import com.ejeplo.productservice.Exception.ResourceNotFoundException;
 import com.ejeplo.productservice.Repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,35 +12,61 @@ import java.util.Optional;
 @Service
 public class ProductServiceImpl implements ProductService{
 
-    @Autowired
-    private ProductRepository productRepository;
+    //Inyección de dependencias a través del constructor (preferido en Spring)
+    final  ProductRepository productRepository;
+
+    // inyeccion por constructor recomendado
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 
     @Override
+    // asegura que el guardado sea de forma atomica
+    @Transactional
     public ProductEntity saveProduct(ProductEntity productEntity) {
         return productRepository.save(productEntity);
     }
 
     @Override
+    // Optimización para métodos de solo lectura
+    @Transactional(readOnly = true)
     public Optional<ProductEntity> findById(Long id) {
         return productRepository.findById(id);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<ProductEntity> findAll() {
         return productRepository.findAll();
     }
 
     @Override
+    @Transactional
     public boolean deleteByIdPro(Long id) {
         if (productRepository.existsById(id)) {
             productRepository.deleteById(id);
             return true;
         }
+        // si no encuentra el id
         return false;
     }
 
     @Override
-    public ProductEntity updateProduct(ProductEntity productEntity) {
-        return productRepository.save(productEntity);
+    @Transactional
+    public ProductEntity updateProduct(ProductEntity productUpEntity) {
+        return productRepository.findById(productUpEntity.getIdProduct())
+                        .map(ProductExist -> {
+                            if(productUpEntity.getNameProduct() != null ){
+                                productUpEntity.setNameProduct(productUpEntity.getNameProduct());
+                            }
+                            if(productUpEntity.getDescriptionProduct() != null ){
+                                productUpEntity.setDescriptionProduct(productUpEntity.getDescriptionProduct());
+                            }
+                            if (productUpEntity.getPriceProduct() != null){
+                                productUpEntity.setPriceProduct(productUpEntity.getPriceProduct());
+                            }
+                            return productRepository.save(ProductExist);
+                        }
+                ).orElseThrow( () -> new ResourceNotFoundException("No se encontro product con ID: "+productUpEntity.getIdProduct()));
     }
 }
